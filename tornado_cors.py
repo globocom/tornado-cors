d33bf0ec7f41d141ee904cf861bb6c39508d884c
Claudio Figueiredo <jcfigueiredo@gmail.com>
2013-02-24 02:31:58 -0300

    autodetect of allowed methods and making MAX_AGE optional with default

--- tornado_cors.py
+++ tornado_cors.py
@@ -3,27 +3,42 @@
 
 import logging
 
-from tornado.web import asynchronous
+from tornado.web import asynchronous, RequestHandler
 
+import inspect
+
+def get_class_that_defined_method(meth):
+    for cls in inspect.getmro(meth.im_class):
+        if meth.__name__ in cls.__dict__: return cls
+    return None
 
 class CorsMixin(object):
 
     CORS_ORIGIN = None
     CORS_HEADERS = None
-    CORS_METHODS = None
-    CORS_MAX_AGE = 86400
 
     def prepare(self):
         if self.CORS_ORIGIN:
             self.set_header('Access-Control-Allow-Origin', self.CORS_ORIGIN)
 
+    def _get_methods(self):
+        methods = []
+        for meth in ['get', 'put', 'post', 'patch', 'delete', 'options']:
+            instance_meth = getattr(self, meth)
+            if not meth:
+                continue
+            handler_class = get_class_that_defined_method(instance_meth)
+            if not handler_class is RequestHandler:
+                methods.append(meth.upper())
+
+        return ", ".join(methods)
+
     @asynchronous
     def options(self, *args, **kwargs):
         if self.CORS_HEADERS:
             self.set_header('Access-Control-Allow-Headers', self.CORS_HEADERS)
-        if self.CORS_METHODS:
-            self.set_header('Access-Control-Allow-Methods', self.CORS_METHODS)
-        if self.CORS_MAX_AGE:
-            self.set_header('Access-Control-Max-Age', self.CORS_MAX_AGE)
+        self.set_header('Access-Control-Allow-Methods', self._get_methods())
+        max_age = getattr(self, 'CORS_MAX_AGE', 86400)
+        self.set_header('Access-Control-Max-Age', max_age)
         self.set_status(204)
         self.finish()
