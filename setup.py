b6527ca195aab51587259fbe8b14d23bcecf252c
Felipe Dornelas <felipead@gmail.com>
2015-03-19 22:57:10 -0300

    Enabling Tornado 4.x support. Working without any known issues. Raising version number to 0.6.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readfile(file_name):
 
 setup(
     name='tornado-cors',
-    version='0.5.0',
+    version='0.6.0',
     description='CORS support for Tornado',
     keywords='tornado cors',
     author='globo.com',
